36660c24de8cce270cd430ab99b811e3fe1187a3
Kyeing Song <sk15910766709@gmail.com>
2022-06-24 15:59:01 -0400

    connect to mondodb

---
+++ .history/Server/Config/db_20220624155330.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = ""
+export const HostName = "LocalHost";
+export const Secret = "someSecret";
---
+++ .history/Server/Config/db_20220624155439.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://sk1513:xJipKWMXjFwOWqBv@cluster1.sxloldf.mongodb.net/books229?retryWrites=true&w=majority"
+export const HostName = "MongoD";
+export const Secret = "someSecret";
